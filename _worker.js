e2ac57ea9241e30d719ce28bf8abbe76e80f4589
bishaowei999 <101491131+bishaowei999@users.noreply.github.com>
2024-08-13 09:35:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,15 +12,12 @@ let timestamp = 4102329600000;//2099-12-31
 
 //节点链接 + 订阅链接
 let MainData = `
-vless://b7a392e2-4ef0-4496-90bc-1c37bb234904@cf.090227.xyz:443?encryption=none&security=tls&sni=edgetunnel-2z2.pages.dev&fp=random&type=ws&host=edgetunnel-2z2.pages.dev&path=%2F%3Fed%3D2048#%E5%8A%A0%E5%85%A5%E6%88%91%E7%9A%84%E9%A2%91%E9%81%93t.me%2FCMLiussss%E8%A7%A3%E9%94%81%E6%9B%B4%E5%A4%9A%E4%BC%98%E9%80%89%E8%8A%82%E7%82%B9
-https://sub.xf.free.hr/auto
 vless://1b1de60b-0380-4687-bd09-524811e60573@104.18.57.201:2095?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP5
 vless://1b1de60b-0380-4687-bd09-524811e60573@104.18.57.201:8080?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP2
 vless://1b1de60b-0380-4687-bd09-524811e60573@104.21.222.32:2086?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP6
 vless://1b1de60b-0380-4687-bd09-524811e60573@172.64.100.62:80?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP3
 vless://1b1de60b-0380-4687-bd09-524811e60573@104.18.57.201:80?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP
 vless://1b1de60b-0380-4687-bd09-524811e60573@172.67.61.168:8080?encryption=none&security=none&sni=20240807.bishaowei.workers.dev&fp=randomized&type=ws&host=20240807.bishaowei.workers.dev&path=%2F%3Fed%3D2048#VIP4
-
 `
 
 let urls = [];
